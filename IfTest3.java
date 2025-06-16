5371862dc913e9bac2c5ce224be049f281690dc2
Ryuto-A <anzai.ryuto0405@gmail.com>
2025-06-16 21:45:10 +0900

    0616 Study

--- IfTest3.java
+++ IfTest3.java
@@ -1,5 +1,13 @@
 public class IfTest3 {
   public static void main(String[] args) {
-    
+    int myAge = 34;
+    if(myAge == 35) {
+      System.out.println("私の年齢は35歳!");
+    } else if(myAge >= 40) {
+      System.out.println("私の年齢は40歳以上です！");
+    } else {
+      System.out.println("私の年齢は35歳でも40歳以上でもないです");
+    }
+    System.out.println("私の出身地は・・・");
   }
 }
