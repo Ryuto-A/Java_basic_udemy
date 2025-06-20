8dee2d8b148eedfbd8e938a3c4756119fa89f39f
Ryuto-A <anzai.ryuto0405@gmail.com>
2025-06-20 23:35:33 +0900

    0620 Study

--- AndOrTest.java
+++ AndOrTest.java
@@ -1,6 +1,6 @@
 public class AndOrTest {
   public static void main(String[] args) {
-    // AND文による条件の接続「且つ」.
+    // AND文による条件の接続「且つ」.「&&」で表す.
     int myAge = 35;           // 年齢
     char myBloodType = 'O';   // 血液型
     if(myAge == 35 && myBloodType == 'B') {    // 年齢と血液型の両方が一致する場合
@@ -14,6 +14,35 @@ public class AndOrTest {
     }
     System.out.println("私の出身地は・・・");
 
-    // OR「または」
+    // OR文による条件の接続「または」　「||」で表す.
+    myAge = 50;           // 年齢
+    myBloodType = 'O';   // 血液型
+    if(myAge == 35 || myBloodType == 'B') {    // 年齢と血液型の片方が一致する場合
+        System.out.println("私の年齢は35歳または血液型はB型です！");
+    } else if (myAge == 35 || myBloodType == 'A') {
+        System.out.println("私の年齢は35歳または血液型はA型です！");
+    } else if(myAge == 40 || myBloodType == 'O') {
+        System.out.println("私の年齢は40歳または血液型はA型です！");
+    } else {
+        System.out.println("私の年齢と血液型は、あなたの考えていることとは異なります！");
+    }
+    System.out.println("私の出身地は・・・");
+  
+    // 補足　String型で比較をする場合は、「＝＝」で繋がない方が良い. 詳細は後の動画.
+    // "AB"型で比較するためには、「＝＝」ではなく「.equals("AB")」にする.
+    myAge = 40;           // 年齢
+    String myBloodType2 = "不明";   // 血液型
+    if(myAge == 35 || myBloodType2.equals("B")) {    // 年齢と血液型の片方が一致する場合
+        System.out.println("私の年齢は35歳または血液型はB型です！");
+    } else if (myAge == 35 || myBloodType2.equals("A")) {
+        System.out.println("私の年齢は35歳または血液型はA型です！");
+    } else if(myAge == 40 || myBloodType2.equals("O")) {
+        System.out.println("私の年齢は40歳または血液型はO型です！");
+    } else if(myAge == 40 || myBloodType2.equals("AB")) {
+        System.out.println("私の年齢は40歳または血液型はAB型です！");
+    } else {
+        System.out.println("私の年齢と血液型は、あなたの考えていることとは異なります！");
+    }
+    System.out.println("私の出身地は・・・");
   }
 }
